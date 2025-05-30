42ec05f1968851b64fa9878bd59153658cb5651f
dogge84@hotmail.com <dogge84@hotmail.com>
2025-05-30 16:21:48 +0200

    update4

--- uppg7.js
+++ uppg7.js
@@ -4,17 +4,15 @@ function uppg7() {
    
     // skapa en funktion som tar in två tal som argument
     // funktionen ska returnera summan av talen
-    
+
     function returnSumFunction(tal1, tal2){
 
-
-    return tal1 + tal2;
+    return (tal1 + tal2);
 }
     // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
 
 
 let addNumbers = returnSumFunction(5, 10);
- 
 }
 
 module.exports = { uppg7 };
