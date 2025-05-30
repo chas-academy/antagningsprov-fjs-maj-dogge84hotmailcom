1ce384c629aae82d47338f531c9d3cc4b9f58df3
dogge84@hotmail.com <dogge84@hotmail.com>
2025-05-30 15:39:57 +0200

    update2

--- uppg2.js
+++ uppg2.js
@@ -1,8 +1,10 @@
 
 function uppg2() {
-   let myName = "Douglas Netzel";
-console.log(myName);
+   
    // deklarera en variable med ditt namn och skriv ut det i konsolen
+let myName = "Douglas Netzel";
+console.log(myName);
+
 
 
 }
